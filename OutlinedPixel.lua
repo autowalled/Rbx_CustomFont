f5f107c3e9ca7c506ede52de2c2b2c5b17d59aa0
integerisqt <86178302+integerisqt@users.noreply.github.com>
2022-05-09 13:54:40 +0700

    Create OutlinedPixel.lua

---
+++ OutlinedPixel.lua
@@ -0,0 +1,137 @@
+--[[
+	@Font Outline Pixel7
+	@Sizes {20}
+	@Author N/A
+	@Link N/A
+--]]
+
+local module = {}
+
+module.atlases = {
+	[1] = "rbxassetid://9584855853";
+	[2] = "rbxassetid://9584855858";
+	[3] = "rbxassetid://9584855867";
+	[4] = "rbxassetid://9584855857";
+	[5] = "rbxassetid://9584855855";
+	[6] = "rbxassetid://9584855854";
+	[7] = "rbxassetid://9584855856";
+	[8] = "rbxassetid://9584855859";
+};
+
+module.font = {
+	information = {
+		family = "Outline Pixel7",
+		styles = {"Regular"},
+		sizes = {20},
+		useEnums = false
+	},
+	styles = {
+		["Regular"] = {
+			["20"] = {
+				lineHeight = 15,
+				firstAdjust = 6,
+				characters = {
+					["32"] = { x = 0, y = 0, width = 0, height = 0, xadvance = 13, yoffset = 20, atlas = 0 },
+					["33"] = { x = 5, y = 0, width = 8, height = 13, xadvance = 13, yoffset = 7, atlas = 0 },
+					["34"] = { x = 18, y = 0, width = 10, height = 5, xadvance = 13, yoffset = 7, atlas = 0 },
+					["35"] = { x = 33, y = 0, width = 13, height = 13, xadvance = 13, yoffset = 7, atlas = 0 },
+					["36"] = { x = 51, y = 0, width = 12, height = 15, xadvance = 13, yoffset = 6, atlas = 0 },
+					["37"] = { x = 0, y = 20, width = 12, height = 12, xadvance = 13, yoffset = 8, atlas = 0 },
+					["38"] = { x = 17, y = 20, width = 13, height = 13, xadvance = 13, yoffset = 7, atlas = 0 },
+					["39"] = { x = 35, y = 20, width = 8, height = 5, xadvance = 13, yoffset = 7, atlas = 0 },
+					["40"] = { x = 48, y = 20, width = 9, height = 13, xadvance = 13, yoffset = 7, atlas = 0 },
+					["41"] = { x = 0, y = 40, width = 9, height = 13, xadvance = 13, yoffset = 7, atlas = 0 },
+					["42"] = { x = 14, y = 40, width = 12, height = 11, xadvance = 13, yoffset = 8, atlas = 0 },
+					["43"] = { x = 31, y = 40, width = 11, height = 9, xadvance = 13, yoffset = 9, atlas = 0 },
+					["44"] = { x = 47, y = 40, width = 9, height = 4, xadvance = 13, yoffset = 17, atlas = 0 },
+					["45"] = { x = 0, y = 0, width = 11, height = 3, xadvance = 13, yoffset = 12, atlas = 1 },
+					["46"] = { x = 16, y = 0, width = 8, height = 3, xadvance = 13, yoffset = 17, atlas = 1 },
+					["47"] = { x = 29, y = 0, width = 12, height = 12, xadvance = 13, yoffset = 8, atlas = 1 },
+					["48"] = { x = 46, y = 0, width = 12, height = 13, xadvance = 13, yoffset = 7, atlas = 1 },
+					["49"] = { x = 0, y = 20, width = 8, height = 13, xadvance = 13, yoffset = 7, atlas = 1 },
+					["50"] = { x = 13, y = 20, width = 12, height = 13, xadvance = 13, yoffset = 7, atlas = 1 },
+					["51"] = { x = 30, y = 20, width = 12, height = 13, xadvance = 13, yoffset = 7, atlas = 1 },
+					["52"] = { x = 47, y = 20, width = 12, height = 13, xadvance = 13, yoffset = 7, atlas = 1 },
+					["53"] = { x = 0, y = 40, width = 12, height = 13, xadvance = 13, yoffset = 7, atlas = 1 },
+					["54"] = { x = 17, y = 40, width = 12, height = 13, xadvance = 13, yoffset = 7, atlas = 1 },
+					["55"] = { x = 34, y = 40, width = 12, height = 13, xadvance = 13, yoffset = 7, atlas = 1 },
+					["56"] = { x = 51, y = 40, width = 12, height = 13, xadvance = 13, yoffset = 7, atlas = 1 },
+					["57"] = { x = 0, y = 0, width = 12, height = 13, xadvance = 13, yoffset = 7, atlas = 2 },
+					["58"] = { x = 17, y = 0, width = 8, height = 9, xadvance = 13, yoffset = 9, atlas = 2 },
+					["59"] = { x = 30, y = 0, width = 9, height = 12, xadvance = 13, yoffset = 9, atlas = 2 },
+					["60"] = { x = 44, y = 0, width = 9, height = 11, xadvance = 13, yoffset = 8, atlas = 2 },
+					["61"] = { x = 0, y = 20, width = 11, height = 7, xadvance = 13, yoffset = 10, atlas = 2 },
+					["62"] = { x = 16, y = 20, width = 9, height = 11, xadvance = 13, yoffset = 8, atlas = 2 },
+					["63"] = { x = 30, y = 20, width = 12, height = 13, xadvance = 13, yoffset = 7, atlas = 2 },
+					["64"] = { x = 47, y = 20, width = 13, height = 13, xadvance = 13, yoffset = 7, atlas = 2 },
+					["65"] = { x = 0, y = 40, width = 12, height = 13, xadvance = 13, yoffset = 7, atlas = 2 },
+					["66"] = { x = 17, y = 40, width = 12, height = 13, xadvance = 13, yoffset = 7, atlas = 2 },
+					["67"] = { x = 34, y = 40, width = 12, height = 13, xadvance = 13, yoffset = 7, atlas = 2 },
+					["68"] = { x = 51, y = 40, width = 12, height = 13, xadvance = 13, yoffset = 7, atlas = 2 },
+					["69"] = { x = 0, y = 0, width = 12, height = 13, xadvance = 13, yoffset = 7, atlas = 3 },
+					["70"] = { x = 17, y = 0, width = 12, height = 13, xadvance = 13, yoffset = 7, atlas = 3 },
+					["71"] = { x = 34, y = 0, width = 12, height = 13, xadvance = 13, yoffset = 7, atlas = 3 },
+					["72"] = { x = 51, y = 0, width = 12, height = 13, xadvance = 13, yoffset = 7, atlas = 3 },
+					["73"] = { x = 0, y = 20, width = 10, height = 13, xadvance = 13, yoffset = 7, atlas = 3 },
+					["74"] = { x = 15, y = 20, width = 12, height = 13, xadvance = 13, yoffset = 7, atlas = 3 },
+					["75"] = { x = 32, y = 20, width = 12, height = 13, xadvance = 13, yoffset = 7, atlas = 3 },
+					["76"] = { x = 49, y = 20, width = 12, height = 13, xadvance = 13, yoffset = 7, atlas = 3 },
+					["77"] = { x = 0, y = 40, width = 12, height = 13, xadvance = 13, yoffset = 7, atlas = 3 },
+					["78"] = { x = 17, y = 40, width = 12, height = 13, xadvance = 13, yoffset = 7, atlas = 3 },
+					["79"] = { x = 34, y = 40, width = 12, height = 13, xadvance = 13, yoffset = 7, atlas = 3 },
+					["80"] = { x = 51, y = 40, width = 12, height = 13, xadvance = 13, yoffset = 7, atlas = 3 },
+					["81"] = { x = 0, y = 0, width = 13, height = 14, xadvance = 13, yoffset = 7, atlas = 4 },
+					["82"] = { x = 18, y = 0, width = 12, height = 13, xadvance = 13, yoffset = 7, atlas = 4 },
+					["83"] = { x = 35, y = 0, width = 12, height = 13, xadvance = 13, yoffset = 7, atlas = 4 },
+					["84"] = { x = 52, y = 0, width = 12, height = 13, xadvance = 13, yoffset = 7, atlas = 4 },
+					["85"] = { x = 0, y = 20, width = 12, height = 13, xadvance = 13, yoffset = 7, atlas = 4 },
+					["86"] = { x = 17, y = 20, width = 12, height = 13, xadvance = 13, yoffset = 7, atlas = 4 },
+					["87"] = { x = 34, y = 20, width = 12, height = 13, xadvance = 13, yoffset = 7, atlas = 4 },
+					["88"] = { x = 51, y = 20, width = 12, height = 13, xadvance = 13, yoffset = 7, atlas = 4 },
+					["89"] = { x = 0, y = 40, width = 12, height = 13, xadvance = 13, yoffset = 7, atlas = 4 },
+					["90"] = { x = 17, y = 40, width = 12, height = 13, xadvance = 13, yoffset = 7, atlas = 4 },
+					["91"] = { x = 34, y = 40, width = 10, height = 13, xadvance = 13, yoffset = 7, atlas = 4 },
+					["92"] = { x = 49, y = 40, width = 12, height = 12, xadvance = 13, yoffset = 8, atlas = 4 },
+					["93"] = { x = 0, y = 0, width = 8, height = 13, xadvance = 13, yoffset = 7, atlas = 5 },
+					["94"] = { x = 13, y = 0, width = 12, height = 6, xadvance = 13, yoffset = 7, atlas = 5 },
+					["95"] = { x = 30, y = 0, width = 12, height = 3, xadvance = 13, yoffset = 17, atlas = 5 },
+					["96"] = { x = 47, y = 0, width = 8, height = 4, xadvance = 13, yoffset = 7, atlas = 5 },
+					["97"] = { x = 0, y = 20, width = 12, height = 13, xadvance = 13, yoffset = 7, atlas = 5 },
+					["98"] = { x = 17, y = 20, width = 12, height = 13, xadvance = 13, yoffset = 7, atlas = 5 },
+					["99"] = { x = 34, y = 20, width = 12, height = 13, xadvance = 13, yoffset = 7, atlas = 5 },
+					["100"] = { x = 51, y = 20, width = 12, height = 13, xadvance = 13, yoffset = 7, atlas = 5 },
+					["101"] = { x = 0, y = 40, width = 12, height = 13, xadvance = 13, yoffset = 7, atlas = 5 },
+					["102"] = { x = 17, y = 40, width = 12, height = 13, xadvance = 13, yoffset = 7, atlas = 5 },
+					["103"] = { x = 34, y = 40, width = 12, height = 13, xadvance = 13, yoffset = 7, atlas = 5 },
+					["104"] = { x = 51, y = 40, width = 12, height = 13, xadvance = 13, yoffset = 7, atlas = 5 },
+					["105"] = { x = 0, y = 0, width = 10, height = 13, xadvance = 13, yoffset = 7, atlas = 6 },
+					["106"] = { x = 15, y = 0, width = 12, height = 13, xadvance = 13, yoffset = 7, atlas = 6 },
+					["107"] = { x = 32, y = 0, width = 12, height = 13, xadvance = 13, yoffset = 7, atlas = 6 },
+					["108"] = { x = 49, y = 0, width = 12, height = 13, xadvance = 13, yoffset = 7, atlas = 6 },
+					["109"] = { x = 0, y = 20, width = 12, height = 13, xadvance = 13, yoffset = 7, atlas = 6 },
+					["110"] = { x = 17, y = 20, width = 12, height = 13, xadvance = 13, yoffset = 7, atlas = 6 },
+					["111"] = { x = 34, y = 20, width = 12, height = 13, xadvance = 13, yoffset = 7, atlas = 6 },
+					["112"] = { x = 51, y = 20, width = 12, height = 13, xadvance = 13, yoffset = 7, atlas = 6 },
+					["113"] = { x = 0, y = 40, width = 13, height = 14, xadvance = 13, yoffset = 7, atlas = 6 },
+					["114"] = { x = 18, y = 40, width = 12, height = 13, xadvance = 13, yoffset = 7, atlas = 6 },
+					["115"] = { x = 35, y = 40, width = 12, height = 13, xadvance = 13, yoffset = 7, atlas = 6 },
+					["116"] = { x = 52, y = 40, width = 12, height = 13, xadvance = 13, yoffset = 7, atlas = 6 },
+					["117"] = { x = 0, y = 0, width = 12, height = 13, xadvance = 13, yoffset = 7, atlas = 7 },
+					["118"] = { x = 17, y = 0, width = 12, height = 13, xadvance = 13, yoffset = 7, atlas = 7 },
+					["119"] = { x = 34, y = 0, width = 12, height = 13, xadvance = 13, yoffset = 7, atlas = 7 },
+					["120"] = { x = 51, y = 0, width = 12, height = 13, xadvance = 13, yoffset = 7, atlas = 7 },
+					["121"] = { x = 0, y = 20, width = 12, height = 13, xadvance = 13, yoffset = 7, atlas = 7 },
+					["122"] = { x = 17, y = 20, width = 12, height = 13, xadvance = 13, yoffset = 7, atlas = 7 },
+					["123"] = { x = 34, y = 20, width = 9, height = 13, xadvance = 13, yoffset = 7, atlas = 7 },
+					["124"] = { x = 48, y = 20, width = 8, height = 13, xadvance = 13, yoffset = 7, atlas = 7 },
+					["125"] = { x = 0, y = 40, width = 10, height = 13, xadvance = 13, yoffset = 7, atlas = 7 },
+					["126"] = { x = 15, y = 40, width = 12, height = 5, xadvance = 13, yoffset = 11, atlas = 7 }
+				},
+				kerning = {
+				}
+			}
+		}
+	}
+}
+
+return module
